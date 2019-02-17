6953bd2e77be999295dcb4887c0b7791b5d088e5
Hugo Tigre <hugo.tigre@gmail.com>
2019-02-17 16:20:31 +0000

    initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Security utils for encryption and decryption
+
+
+Important: this service has <a href="https://github.com/abstractj/kalium">kalium</a> as a dependency,
+which itself needs <a href="https://github.com/jedisct1/libsodium">libsodium</a> installed
+directly in the operating system for this to work. For more on this please read the Kalium documentation.
+
+<a href="http://macappstore.org/libsodium/">Install libsodium in macos</a>
---
+++ src/main/java/com/pakybytes/sys/services/encryption/Nonce.java
@@ -0,0 +1,69 @@
+package com.pakybytes.sys.services.encryption;
+
+import org.abstractj.kalium.crypto.Random;
+
+import static org.abstractj.kalium.NaCl.Sodium.CRYPTO_SECRETBOX_XSALSA20POLY1305_NONCEBYTES;
+
+
+/**
+ * A Nonce is used to ensure that encryption is completely random.
+ * They should be generated once per encryption.
+ * <p>
+ * Nonce's can be displayed, they are not confidential but
+ * should never be reused, ever.
+ *
+ * @author Hugo Tigre
+ */
+public class Nonce {
+
+    private static Random random = new Random(); // No real advantage over java.secure.SecureRandom
+    private byte[] raw;
+
+
+    private Nonce(byte[] raw) {
+        this.raw = raw;
+    }
+
+
+    public byte[] getRaw() {
+        return this.raw;
+    }
+
+
+    public static class Builder {
+
+        private byte[] bytes;
+
+
+        /**
+         * Set the bytes of the Nonce, if not specified, a random set of 24 bytes
+         * will be generated.
+         *
+         * @param data byte array to feed the Nonce
+         * @return The Builder
+         */
+        public Builder fromBytes(byte[] data) {
+
+            if (data == null) throw new NullPointerException();
+
+            if (data.length != CRYPTO_SECRETBOX_XSALSA20POLY1305_NONCEBYTES) {
+                throw new IllegalArgumentException("This nonce has an invalid size: " + data.length);
+            }
+
+            bytes = data;
+            return this;
+        }
+
+
+        /**
+         * Creates a random nonce value of 24 bytes.
+         *
+         * @return a new instance of Nonce
+         */
+        public Nonce build() {
+            return bytes == null ?
+                    new Nonce(random.randomBytes(CRYPTO_SECRETBOX_XSALSA20POLY1305_NONCEBYTES)) :
+                    new Nonce(bytes);
+        }
+    }
+}
